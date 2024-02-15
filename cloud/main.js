c0631a61ede65484a94225b8ca7e234e27c39064
leoCardosoDev <leocardosodev@gmail.com>
2024-02-15 09:01:58 -0300

    feat: basics query

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,8 @@
-const Professional = Parse.Object.extend("Professional");
-Parse.Cloud.define("v1-get-professionals", (request) => {
-	return "Hello world!";
-});
+const Professional = Parse.Object.extend("Professional")
+Parse.Cloud.define("v1-get-professionals", async (request) => {
+	const query = new Parse.Query(Professional)
+  query.equalTo("crm", "123")
+  query.include(['specialties', 'services'])
+  const results = await query.find({useMasterKey: true})
+  return results
+})
